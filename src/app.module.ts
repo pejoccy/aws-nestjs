19d5f7a425f4229a95e5396d23ed5719b445ab12
Segun Sodimu <princesegzy01@yahoo.com>
2022-06-03 15:08:09 +0100

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { NotificationController } from './notification/notification.controller';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, UserModule, NotificationModule],
+  controllers: [AppController, NotificationController],
   providers: [AppService],
 })
 export class AppModule {}
